35bfc90e4cbd8cdca534ef3d7aeefe0844faecd1
Charity Bunyon <cseymou5@gmail.com>
2020-01-04 15:18:55 -0600

    figured out how to render icons

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 class Home extends React.Component {
   render() {
@@ -10,6 +12,7 @@ class Home extends React.Component {
         <h1>Home Page</h1>
         <Link className="btn btn-danger" to="/board/new">Create New Board</Link>
         <Link className="btn btn-primary" to={`/board/${boardId}`}>Single Board Page</Link>
+        <FontAwesomeIcon icon={faCoffee} />
       </div>
     );
   }
