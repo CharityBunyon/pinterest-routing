f13056a4db4a1e981931ba6f5e4f1314d52b30b4
Charity Bunyon <cseymou5@gmail.com>
2020-01-07 19:53:09 -0600

    boards appear

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './Home.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import boardData from '../../../helpers/data/boardData';
+import authData from '../../../helpers/data/authData';
+import Board from '../../shared/Board/Board';
 
 class Home extends React.Component {
+  state = {
+    boards: [],
+  }
+
+  componentDidMount() {
+    boardData.getBoardsByUid(authData.getUid())
+      .then((boards) => this.setState({ boards }))
+      .catch((err) => console.error('error from get boards', err));
+  }
+
   render() {
-    const boardId = '12345';
     return (
       <div className="Home">
         <h1>Home Page</h1>
-        <Link className="btn btn-danger" to="/board/new">Create New Board</Link>
-        <Link className="btn btn-primary" to={`/board/${boardId}`}>Single Board Page</Link>
         <FontAwesomeIcon icon={faCoffee} />
+        <div className="boards d-flex flex-wrap">
+          {this.state.boards.map((board) => <Board key={board.id} board={board} />)}
+        </div>
       </div>
     );
   }
